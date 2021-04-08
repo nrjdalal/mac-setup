d56a5ad5530a0afdb8f1f0b61ba82e80d77caee5
Neeraj Dalal <nd941z@gmail.com>
2021-04-08 12:52:28 +0530

    Update mac-setup.sh

--- mac-setup.sh
+++ mac-setup.sh
@@ -1,4 +1,5 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> .zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew install git
