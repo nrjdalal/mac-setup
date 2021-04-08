5eb6623ad683c37fa0a03b6454b4eea9c47d2325
Neeraj Dalal <nd941z@gmail.com>
2021-04-08 14:55:38 +0530

    Update mac-setup.sh

--- mac-setup.sh
+++ mac-setup.sh
@@ -1,4 +1,4 @@
-sudo softwareupdate --install-rosetta --agree-to-license
+softwareupdate --install-rosetta --agree-to-license
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" << INPUT
 
