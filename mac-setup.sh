7f728233024c6c99a75d232ed50db08be094b12c
Neeraj Dalal <nd941z@gmail.com>
2021-04-08 21:42:46 +0530

    Update mac-setup.sh

--- mac-setup.sh
+++ mac-setup.sh
@@ -11,6 +11,15 @@ brew install git
 brew install node
 brew install wget
 
+brew install --cask balenaetcher
+brew install --cask bartender
+brew install --cask keka
+brew install --cask rectangle
+brew install --cask stats
+
+brew install --cask telegram
+brew install --cask whatsapp
+
 brew install --cask elmedia-player
 brew install --cask figma
 brew install --cask firefox
@@ -20,19 +29,12 @@ brew install --cask mongodb-compass
 brew install --cask obs
 brew install --cask postman
 brew install --cask qbittorrent
-brew install --cask telegram
 brew install --cask typora
 brew install --cask visual-studio-code
-brew install --cask whatsapp
 
 brew tap mongodb/brew
 brew install mongodb-community
 brew services start mongodb-community
-
-brew install --cask bartender
-brew install --cask keka
-brew install --cask rectangle
-brew install --cask stats
 
 wget -O ~/Downloads/designer.dmg 'https://store.serif.com/download/aa4dee/'
 hdiutil mount ~/Downloads/designer.dmg
