2d8edd848086d6923ee98610649f78da9bf0d3df
Neeraj Dalal <nd941z@gmail.com>
2021-04-08 21:00:01 +0530

    Update mac-setup.sh

--- mac-setup.sh
+++ mac-setup.sh
@@ -35,8 +35,19 @@ brew install --cask rectangle
 brew install --cask stats
 
 wget -O ~/Downloads/designer.dmg 'https://store.serif.com/download/aa4dee/'
-wget -O ~/Downloads/photo.dmg 'https://store.serif.com/download/075b51/'
-wget -O ~/Downloads/publisher.dmg 'https://store.serif.com/download/7ef252/'
 hdiutil mount ~/Downloads/designer.dmg
+sudo cp -R "/Volumes/Affinity Designer/Affinity Designer.app" /Applications
+hdiutil unmount '/Volumes/Affinity Designer'
+rm ~/Downloads/designer.dmg
+
+wget -O ~/Downloads/photo.dmg 'https://store.serif.com/download/075b51/'
 hdiutil mount ~/Downloads/photo.dmg
+sudo cp -R "/Volumes/Affinity Photo/Affinity Photo.app" /Applications
+hdiutil unmount '/Volumes/Affinity Photo'
+rm ~/Downloads/photo.dmg
+
+wget -O ~/Downloads/publisher.dmg 'https://store.serif.com/download/7ef252/'
 hdiutil mount ~/Downloads/publisher.dmg
+sudo cp -R "/Volumes/Affinity Publisher/Affinity Publisher.app" /Applications
+hdiutil unmount '/Volumes/Affinity Publisher'
+rm ~/Downloads/publisher.dmg
