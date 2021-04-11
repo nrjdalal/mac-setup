5b277518c6d5a798dc16e5d249ab8289f3f779c7
Neeraj Dalal <nd941z@gmail.com>
2021-04-11 19:44:36 +0530

    Update mac-setup.sh

--- mac-setup.sh
+++ mac-setup.sh
@@ -1,51 +1,5 @@
 sudo softwareupdate --install-rosetta --agree-to-license
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" << INPUT
-
-INPUT
-
-echo 'export PATH=/opt/homebrew/bin:$PATH' >> ~/.zshrc
-source ~/.zshrc
-
-brew install git
-brew install node@14
-brew install wget
-brew install youtube-dl
-
-brew install --cask balenaetcher
-brew install --cask bartender
-brew install --cask blackhole-2ch
-brew install --cask iterm2
-brew install --cask jettison
-brew install --cask keka
-brew install --cask rectangle
-brew install --cask stats
-
-brew install --cask cakebrew
-brew install --cask figma
-brew install --cask firefox
-brew install --cask fontbase
-brew install --cask github
-brew install --cask google-chrome
-brew install --cask grammarly
-brew install --cask handbrake
-brew install --cask iina
-brew install --cask mongodb-compass
-brew install --cask obs
-brew install --cask obsidian
-brew install --cask postman
-brew install --cask qbittorrent
-brew install --cask spotify
-brew install --cask tor-browser
-brew install --cask typora
-brew install --cask telegram
-brew install --cask visual-studio-code
-brew install --cask whatsapp
-
-brew tap mongodb/brew
-brew install mongodb-community
-brew services start mongodb-community
-
 wget -O ~/Downloads/designer.dmg 'https://store.serif.com/download/aa4dee/'
 hdiutil mount ~/Downloads/designer.dmg
 sudo cp -R "/Volumes/Affinity Designer/Affinity Designer.app" /Applications
@@ -63,3 +17,48 @@ hdiutil mount ~/Downloads/publisher.dmg
 sudo cp -R "/Volumes/Affinity Publisher/Affinity Publisher.app" /Applications
 hdiutil unmount '/Volumes/Affinity Publisher'
 rm ~/Downloads/publisher.dmg
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" << INPUT
+INPUT
+
+echo 'export PATH=/opt/homebrew/bin:$PATH' >> ~/.zshrc
+source ~/.zshrc
+
+brew install git
+brew install node@14
+brew install wget
+brew install youtube-dl
+
+brew install --cask balenaetcher
+# brew install --cask bartender
+brew install --cask blackhole-2ch
+# brew install --cask jettison
+# brew install --cask keka
+brew install --cask rectangle
+brew install --cask stats
+
+brew install --cask cakebrew
+brew install --cask figma
+brew install --cask firefox
+brew install --cask fontbase
+brew install --cask github
+brew install --cask google-chrome
+brew install --cask grammarly
+brew install --cask handbrake
+brew install --cask iina
+brew install --cask iterm2
+brew install --cask mongodb-compass
+brew install --cask obs
+brew install --cask obsidian
+brew install --cask postman
+brew install --cask qbittorrent
+brew install --cask spotify
+brew install --cask telegram
+brew install --cask tor-browser
+brew install --cask typora
+brew install --cask visual-studio-code
+brew install --cask whatsapp
+
+brew tap mongodb/brew
+brew install mongodb-community
+brew services start mongodb-community
